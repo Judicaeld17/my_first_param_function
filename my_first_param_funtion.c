fb1bf56348a8c105a8b25431078b7eb3687ce7ce
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-03-05 18:51:28 +0100

    first commit

---
+++ my_first_param_funtion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void detonation_in(int seconds_left){
+    printf("detonation in... %d seconds.\n",seconds_left);
+}
+int main(){
+int timer =10;
+while(timer>0){
+        detonation_in(timer);
+        timer--;
+    }
+}
